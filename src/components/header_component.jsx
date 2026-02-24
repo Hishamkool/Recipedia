ccdb71d198800f93bdd1f156114bc7d1ef76694e
Hisham <linkedin.hisham@gmail.com>
2026-02-25 00:27:00 +0530

    header

--- src/components/header_component.jsx
+++ src/components/header_component.jsx
@@ -3,13 +3,22 @@ import React from "react";
 function HeaderComponent() {
   return (
     <>
-      <header className="bg-white h-52 w-full px-2.5 py-1.5 shadow-xl">
+      <header className="bg-white h-52 w-full px-2.5 py-1.5 shadow-xl top-0">
         <div>
-          <h1 className="text-5xl ">Recipedia</h1>
-          <div className="flex justify-center items-center bg-amber-50 my-10">
+          <div className="flex flex-row justify-between gap-1.5 items-end">
+            <h1 className="text-5xl ">Recipedia</h1>
+            <button
+              className="group: ring-1  ring-amber-600
+            hover:bg-red-400 text-red-400 hover:text-amber-50
+            py-1.5 px-2.5 rounded-xl shadow-2xs shadow-amber-600"
+            >
+              <span className=" font-semibold text-center">Favourites</span>
+            </button>
+          </div>
+          <div className="flex justify-center items-center  mt-14 ">
             <input
               type="text"
-              className="text-2xl border-2 border-amber-500 rounded-md "
+              className="text-2xl border-2 rounded-md px-2.5 py-1 w-screen mx-20"
               placeholder="Search Recipies..."
               name="search-recipe"
               id="search-recipe"
