b25abb1ee837c111e58951113f2206106393899d
Hisham <linkedin.hisham@gmail.com>
2026-02-27 11:29:40 +0530

    Added pwa and icons

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -10,6 +11,32 @@ export default defineConfig({
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Recipedia",
+        short_name: "Recipedia",
+        description:
+          "My Progressive Web App built with Vite and React.js and tailwind css with meal api",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192-recipe.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512-recipe.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
